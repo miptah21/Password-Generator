1c5814d81a620796808389ef2bf83eaa77423770
miptah21 <miftahudinakbar21@gmail.com>
2023-02-07 22:08:53 +0700

    Add High Level

--- Password-Generator.py
+++ Password-Generator.py
@@ -9,13 +9,22 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-password = ""
+password_list = []
+random.shuffle(letters)
+random.shuffle(symbols)
+random.shuffle(numbers)
 
-for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
-for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
-for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+for char in range(0, nr_letters + 1):
+    password_list.append(letters[char])
+for char in range(0, nr_symbols + 1):
+    password_list.append(symbols[char])
+for char in range(0, nr_numbers + 1):
+    password_list.append(numbers[char])
+
+password = ""
+random.shuffle(password_list)
+
+for i in password_list:
+    password += i
 
 print(f"Here is your password: {password}")
